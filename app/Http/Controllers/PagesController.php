e31bb7d1580ec8d1989cdf070ba10149655eddf9
Evans <evansroysir@gmail.com>
2023-06-02 19:37:21 +0300

    Initial Commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index (){
+        $title = 'Welcom to Octal blog';
+        return view ('pages.index', compact('title'));
+    }
+
+    public function about (){
+        return view ('pages.about');
+    }
+
+    public function services () {
+        return view ('pages.services');
+    }
+}
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Service Page</h1>
+    <p>This is the service Page</p>
+@endsection
