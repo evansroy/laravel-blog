40145e4e8154d9a75471ed7bc52052de6d4823c7
Evans <evansroysir@gmail.com>
2023-06-07 23:09:36 +0300

    Add edit button

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,8 +4,10 @@
     <a href="/posts" class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">Go Back</a>
     <h3 class="text-3xl font-bold">{{$post->title}}</h3>
     <div>
-        {{$post->body}}
+        {!!$post->body!!}
     </div>
     <hr>
     <small>Written on {{$post->created_at}}</small>
+    <hr class="mb-5">
+    <a href="/posts/{{$post->id}}/edit" class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">Edit</a>
 @endsection
